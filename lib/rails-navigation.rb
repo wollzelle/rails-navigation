ceb8b5eebde1cdfebeab2240621ad462f38ead36
meleyal <william.meleyal@gmail.com>
2012-01-24 11:52:37 +0100

    fixes

--- lib/rails-navigation.rb
+++ lib/rails-navigation.rb
@@ -6,16 +6,16 @@ module Rails
       protected
 
       def add_navigation(options = {})
-        nav_name, title, url, options = {:nav => :default, :title => "", :url => "", :options => {}}.merge(options).values
+        nav, title, url, options = { :nav => :default, :title => "", :url => "", :options => {} }.merge(options).values
         @navigation ||= {}
         url = send(url) if url.is_a?(Symbol)
-        @navigation[nav_name] ||= []
-        @navigation[nav_name] << [name, url, options]
+        @navigation[nav] ||= []
+        @navigation[nav] << [title, url, options]
       end
 
-      def self.add_navigation(nav_name, name, url, options)
+      def self.add_navigation(nav, title, url, options)
         before_filter options do |controller|
-          controller.send(:add_navigation, nav_name, name, url, options)
+          controller.send(:add_navigation, nav, title, url, options)
         end
       end
 
@@ -23,14 +23,14 @@ module Rails
 
     module Helper
 
-      def navigation?(nav_name)
+      def navigation?(nav)
         return false unless @navigation
-        !(@navigation)[nav_name].nil?
+        !(@navigation)[nav].nil?
       end
       
-      def navigation(nav_name, tag = :li)
-        (@navigation[nav_name] || []).map do |txt, path, options|
-          link = link_to_unless path.blank?, h(txt), path, options
+      def navigation(nav, tag = :li)
+        (@navigation[nav] || []).map do |text, path, options|
+          link = link_to_unless path.blank?, h(text), path, options
           content_tag(tag, link) if tag
         end.join.html_safe
       end
--- rails-navigation.gemspec
+++ rails-navigation.gemspec
@@ -1,11 +1,11 @@
 Gem::Specification.new do |s|
   s.name = 'rails-navigation'
-  s.version = '1.0.3'
+  s.version = '1.0.4'
   s.authors = ["Thomas Koenig", "William Meleyal"]
   s.email = "team@wollzelle.com"
   s.homepage = "http://wollzelle.com"
   s.homepage = 'https://github.com/wollzelle/rails-navigation'
-  s.summary = 'Navigation for for Rails Applications'
+  s.summary = 'Navigation for Rails applications'
   s.add_dependency('rails', '>= 3.0.0')
   s.files = Dir['lib/**/*']
   s.require_path = 'lib'
